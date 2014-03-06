ca0d78110fd62b2ea16fa3aa459a6ae3b80f1036
Zheng Hui Er <erzhenghui@gmail.com>
2014-03-06 16:32:50 +0800

    Communicator update - connection with raspberry pi

--- src/mdp/competition/Communicator.java
+++ src/mdp/competition/Communicator.java
@@ -1,40 +1,42 @@
 package mdp.competition;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+
 import mdp.algo.VirtualCommunicator;
+
 
 public class Communicator extends VirtualCommunicator {
 
     String host = "192.168.4.4";
-
+    int BUFFER_SIZE = 1024;
     int port = 4014;
     String testmessage = "wifi handshake";
 
     boolean termination = false;
+    public static String message;
+    public int[] uS;
+    public int lS;
+    public int rS;
 
     public Communicator() throws IOException{
-        // handshake
 
+
+        // handshake
         try{
-            System.out.println("handshaking...");
+            //System.out.println("handshaking...");
             Socket socket = new Socket(host,port);
-            System.out.println("handshaked");
 
             DataOutputStream outp = new DataOutputStream(socket.getOutputStream());
 
             outp.writeBytes(testmessage);
+
             outp.flush();
             socket.close();
+            //System.out.println("handshaked");
 
         } catch (UnknownHostException e){
             System.err.println(e);
@@ -42,52 +44,54 @@ public class Communicator extends VirtualCommunicator {
         } catch (IOException e){
             System.err.println("Couldn't get I/O connection and handshake with " + host);
             System.exit(1);
-        } //end handshake
+        }
+
+        //end handshake
 
 
-        // server connection
-        int count = 0;
 
-        try(ServerSocket serverSocket = new ServerSocket(port)) {
 
-            System.out.println("Server Up!");
-            while (true){
+
+        // start server to receive message from Raspberry Pi
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
                 try {
-                    Socket clientSocket = serverSocket.accept();
+                    ServerSocket serverSocket = new ServerSocket(4014);
+                    System.out.println("Server Up!");
+                    while (true){
+                        try{
+                            Socket clientSocket = serverSocket.accept();
+                            long startTime = System.currentTimeMillis();
+                            byte[] buffer = new byte[BUFFER_SIZE];
+                            int read;
+                            int totalRead = 0;
+                            InputStream clientInputStream = clientSocket.getInputStream();
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientInputStream));
+                            message = bufferedReader.readLine();
+                            System.out.println("Message Received: "+message);
+                            sendMessage("I received your message: "+ message);
+                            parser(message);
 
-                    count++;
-                    System.out.println("#" + count + " from " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
 
-                    HostThread myHostThread = new HostThread(clientSocket, count);
-                    myHostThread.start();
+                            long endTime = System.currentTimeMillis();
+                            System.out.println(totalRead + " bytes read in " + (endTime - startTime) + " ms.");
 
-                } catch (IOException ex) {
-                    System.out.println(ex.toString());
+                        } catch (IOException e){
+                            System.err.println(e.toString());
+                        }
+                    }
+
+                } catch (IOException e){
+                      System.err.println(e.toString());
                 }
-
             }
+        }).start();
 
 
-        } catch (IOException e){
-            System.err.println("Couldn't set up server");
-            System.exit(1);
-        }
-        // server connection
 
 
-        // client connection
-        try(Socket cSocket = new Socket(host,port)){
-            System.out.println("Connected to raspberry pi");
-
-            DataOutputStream outp = new DataOutputStream(cSocket.getOutputStream());
-            outp.writeBytes(testmessage);
-            outp.flush();
-
-        } catch (IOException ex) {
-            System.out.println(ex.toString());
-        }
     }
-
 
 
 
@@ -97,97 +101,51 @@ public class Communicator extends VirtualCommunicator {
         termination = true;
     }
 
+
+    // Send Message to Raspberry Pi
     public void sendMessage(String message) throws IOException{
-        try(Socket cSocket = new Socket(host, port)){
-            DataOutputStream out = new DataOutputStream(cSocket.getOutputStream());
-            out.writeBytes(message);
-            out.flush();
-            cSocket.close();
+        final String sentMessage;
+        sentMessage = message;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
 
-        }
+                    Socket socket = new Socket(host, port);
+                    System.out.println("Connected to "+host);
+
+
+                    OutputStream socketOutputStream = socket.getOutputStream();
+                    long startTime = System.currentTimeMillis();
+
+                    socketOutputStream.write(sentMessage.getBytes());
+                    System.out.println(" messaged sent");
+
+
+                    socketOutputStream.close();
+
+                    socket.close();
+                    long endTime = System.currentTimeMillis();
+                    System.out.println(" bytes written in " + (endTime - startTime) + " ms.");
+                } catch (Exception e) {
+                    System.err.println(e.toString());
+                }
+            }
+        }).start();
+
     }
 
-    private static class ClientThread extends Thread{
-
-        private Socket clientThreadSocket;
-        int cnt;
-
-        ClientThread(Socket socket, int c) {
-            clientThreadSocket = socket;
-            cnt = c;
-        }
-
-        @Override
-        public void run() {
-            OutputStream outputStream;
-
-            try{
-                outputStream = clientThreadSocket.getOutputStream();
-
-                try (PrintStream printStream = new PrintStream(outputStream)){
-                    printStream.print("hello"+cnt);
-                }
-                finally {
-                    try{
-                        clientThreadSocket.close();
-                    } catch (IOException ex){
-                        System.out.println(ex.toString());
-                    }
-                }
-
-            }catch (IOException ex) {
-                System.out.println(ex.toString());
-            }
-        }
-    }
-
-    private static class HostThread extends Thread{
-
-        private Socket hostThreadSocket;
-        int cnt;
-
-        HostThread(Socket socket, int c) {
-            hostThreadSocket = socket;
-            cnt = c;
-        }
-
-        @Override
-        public void run() {
-            OutputStream outputStream;
-            InputStream inputStream;
-            try {
-                inputStream = hostThreadSocket.getInputStream();
 
 
+    public void parser(String message){
+    //JSON Parser
 
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(inputStream)) ){
-                    System.out.println("Message received from Raspberry: "+ in.readLine());
-                } catch (IOException ex){
-                    System.out.println(ex.toString());
-                }
-                /*
-                outputStream = hostThreadSocket.getOutputStream();
-                try (PrintStream printStream = new PrintStream(outputStream)){
-                    printStream.print("Hello, you are #" +cnt);
-                }*/
-
-                finally {
-                    try{
-                        hostThreadSocket.close();
-                    } catch (IOException ex) {
-                        System.out.println(ex.toString());
-                    }
-                }
-            }catch (IOException ex){
-                System.out.println(ex.toString());
-
-            }
-        }
     }
 
     @Override
     public int[] ultraSonic() {
         int [] detect = new int[3];
+        detect = uS;
 
         return detect;
     }
@@ -195,13 +153,17 @@ public class Communicator extends VirtualCommunicator {
     @Override
     public int leftSensor() {
         // TODO Auto-generated method stub
-        return 0;
+        int  detect;
+        detect = lS;
+        return detect;
     }
 
     @Override
     public int rightSensor() {
         // TODO Auto-generated method stub
-        return 0;
+        int  detect;
+        detect = rS;
+        return detect;
     }
 
 
